c7612910276cdaf107a7844652abd121875372a9
Moustafa-10 <ayman.moustafa14@gmail.com>
2025-05-11 13:08:48 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\College\\Mobile Computing\\AndroidStudio\\Flutter-SDK\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\flutter_ai_writing_assistant-main\\flutter_ai_writing_assistant-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\College\\Mobile Computing\\AndroidStudio\\Flutter-SDK\\flutter"
+  "PROJECT_DIR=E:\\flutter_ai_writing_assistant-main\\flutter_ai_writing_assistant-main"
+  "FLUTTER_ROOT=E:\\College\\Mobile Computing\\AndroidStudio\\Flutter-SDK\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\flutter_ai_writing_assistant-main\\flutter_ai_writing_assistant-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\flutter_ai_writing_assistant-main\\flutter_ai_writing_assistant-main"
+  "FLUTTER_TARGET=E:\\flutter_ai_writing_assistant-main\\flutter_ai_writing_assistant-main\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\flutter_ai_writing_assistant-main\\flutter_ai_writing_assistant-main\\.dart_tool\\package_config.json"
+)
